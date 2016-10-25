65e543059494251cec7b703d08e7628a21d51e5b
marresgit <saeztoro.mauricio@gmail.com>
2016-10-25 09:53:28 +0200

    Fixed some Error Alerts, Background=CHECK, Rules for _form(post)

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ApplicationRecord
+  validates :message, presence: true,
+  				   length: { :within => 2..20,
+  							 :too_short => 'too short',
+  							 :too_long => 'too long'}		   
+  
+
 end
