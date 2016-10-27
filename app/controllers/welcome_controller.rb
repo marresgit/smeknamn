61098d7f3020929dc1988b793ec912b25621115a
marresgit <saeztoro.mauricio@gmail.com>
2016-10-27 13:15:27 +0200

    Basic auth on admin site

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,18 +1,18 @@
 class WelcomeController < ApplicationController
-
+  http_basic_authenticate_with name: "adminlocal", password: "secretsofrails", :except => [:homepage]
+  
   def homepage
     @skip_navadmin = true
     if params[:search]
       @nickname = Nickname.search(params[:search])
       @namenick = Namenick.search(params[:search])    	
-    else
+    else  
       @nickname = Nickname.all
       @namenick = Namenick.all
     end
 
   end
 
-http_basic_authenticate_with name: "adminlocal", password: "secretsofrails"
   def admin
     @nickname = Nickname.all
     @namenick = Namenick.all
