a474464a1f387b77666ba44d044f5cd1ce3735af
marresgit <saeztoro.mauricio@gmail.com>
2016-11-10 14:21:48 +0100

    Changed some text to swedish, centred the results in the

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   validates :message, presence: true,
-  				   length: { :within => 2..20,
+  				   length: { :within => 2..50,
   							 :too_short => 'too short',
   							 :too_long => 'too long'}		   
   
